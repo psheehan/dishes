0417acb3172eff51daf3ba2e1423d7631909ddd7
Patrick Sheehan <psheehan287@gmail.com>
2019-01-16 17:58:49 -0600

    Souped up setup.py so now dishes should be installable in a normal way.

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from Cython.Build import cythonize
 
 libinterferometry = cythonize([Extension('dishes.interferometry.libinterferometry',["dishes/interferometry/libinterferometry.pyx"],libraries=["m"],extra_compile_args=['-ffast-math'])])[0]
 
-#libinterferometry = cythonize("dishes/interferometry/libinterferometry.pyx")[0]
-#libimaging = cythonize("dishes/imaging/libimaging.pyx")[0]
-
 libimaging = cythonize([Extension('dishes.imaging.libimaging',["dishes/imaging/libimaging.pyx"],libraries=[],extra_compile_args=[])])[0]
 
-setup(ext_modules=[libinterferometry, libimaging])
+setup(name="dishes", version="0.0.1", packages=["dishes","dishes.constants", \
+        "dishes.imaging","dishes.interferometry", \
+        "dishes.spectroscopy"], ext_modules=[libinterferometry, libimaging])
