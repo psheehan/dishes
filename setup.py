9bb1249c8f917c33f049b4ecd63ef971467004ac
Patrick Sheehan <psheehan287@gmail.com>
2019-01-16 21:57:39 -0600

    Cleaned up setup.py a little.

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
 from numpy.distutils.core import setup, Extension
 from Cython.Build import cythonize
 
-libinterferometry = cythonize([Extension('dishes.interferometry.libinterferometry',["dishes/interferometry/libinterferometry.pyx"],libraries=["m"],extra_compile_args=['-ffast-math'])])[0]
+# Set up the extension modules.
 
-libimaging = cythonize([Extension('dishes.imaging.libimaging',["dishes/imaging/libimaging.pyx"],libraries=[],extra_compile_args=[])])[0]
+libinterferometry = cythonize([\
+        Extension('dishes.interferometry.libinterferometry',\
+            ["dishes/interferometry/libinterferometry.pyx"],\
+            libraries=["m"], extra_compile_args=['-ffast-math'])])[0]
 
-setup(name="dishes", version="0.0.1", packages=["dishes","dishes.constants", \
-        "dishes.imaging","dishes.interferometry", \
-        "dishes.spectroscopy"], ext_modules=[libinterferometry, libimaging])
+libimaging = cythonize([Extension('dishes.imaging.libimaging',\
+        ["dishes/imaging/libimaging.pyx"], libraries=[], \
+        extra_compile_args=[])])[0]
+
+setup(name="dishes", version="1.0.0", \
+        packages=[\
+        "dishes",\
+        "dishes.constants", \
+        "dishes.imaging",\
+        "dishes.interferometry", \
+        "dishes.spectroscopy",\
+        "dishes.table"], \
+        ext_modules=[libinterferometry, libimaging])
