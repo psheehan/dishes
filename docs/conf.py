01dc942e357d5bf5d6e28f02752fd4e6edfed258
Patrick Sheehan <psheehan287@gmail.com>
2021-11-08 07:42:04 -0600

    Update readthedocs requirements to match local build that works.

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Patrick Sheehan'
 author = 'Patrick Sheehan'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.0'
+release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
