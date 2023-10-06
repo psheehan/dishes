psheehan/pdspy

Turned Image into a Cython class. Also added a routine to calculate uncertainty from a provided image.

setup.py
@@ -2,7 +2,8 @@ from numpy.distutils.core import setup, Extension
 from Cython.Build import cythonize
 
 libinterferometry = cythonize("pdspy/interferometry/libinterferometry.pyx")[0]
+libimaging = cythonize("pdspy/imaging/libimaging.pyx")[0]
 bhmie = Extension('pdspy.dust.bhmie', sources=['pdspy/dust/bhmie.f90'])
 bhcoat = Extension('pdspy.dust.bhcoat', sources=['pdspy/dust/bhcoat.f90'])
 
-setup(ext_modules=[libinterferometry, bhmie, bhcoat])
+setup(ext_modules=[libinterferometry, libimaging, bhmie, bhcoat])

psheehan/dishes

setup.py
@@ -2,5 +2,6 @@ from numpy.distutils.core import setup, Extension
 from Cython.Build import cythonize
 
 libinterferometry = cythonize("dishes/interferometry/libinterferometry.pyx")[0]
+libimaging = cythonize("dishes/imaging/libimaging.pyx")[0]
 
-setup(ext_modules=[libinterferometry])
+setup(ext_modules=[libinterferometry, libimaging])
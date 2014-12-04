5cffdaf8b62198137b778c7e79078311745ac31c
Patrick Sheehan <psheehan287@gmail.com>
2014-12-03 21:01:23 -0700

    Turned Image into a Cython class. Also added a routine to calculate uncertainty from a provided image.

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from numpy.distutils.core import setup, Extension
 from Cython.Build import cythonize
 
 libinterferometry = cythonize("dishes/interferometry/libinterferometry.pyx")[0]
+libimaging = cythonize("dishes/imaging/libimaging.pyx")[0]
 
-setup(ext_modules=[libinterferometry])
+setup(ext_modules=[libinterferometry, libimaging])
