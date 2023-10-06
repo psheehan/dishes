11498945cce8ac3605778fec9d04bfccfa9a88e7
Patrick Sheehan <psheehan287@gmail.com>
2023-10-06 11:52:18 -0500

    Touch ups in setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from numpy.distutils.core import setup, Extension
 from Cython.Build import cythonize
 
 libinterferometry = cythonize([Extension('dishes.interferometry.libinterferometry',["dishes/interferometry/libinterferometry.pyx"],libraries=["m"],extra_compile_args=['-ffast-math'])])[0]
+
 #libinterferometry = cythonize("dishes/interferometry/libinterferometry.pyx")[0]
 #libimaging = cythonize("dishes/imaging/libimaging.pyx")[0]
+
 libimaging = cythonize([Extension('dishes.imaging.libimaging',["dishes/imaging/libimaging.pyx"],libraries=[],extra_compile_args=[])])[0]
 
 setup(ext_modules=[libinterferometry, libimaging])
