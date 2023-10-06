psheehan/pdspy

Made some changes to chisq to make it run significantly faster. Had to update setup.py too to add a compiler flag.

setup.py
@@ -1,7 +1,8 @@
 from numpy.distutils.core import setup, Extension
 from Cython.Build import cythonize
 
-libinterferometry = cythonize("pdspy/interferometry/libinterferometry.pyx")[0]
+libinterferometry = cythonize([Extension('pdspy.interferometry.libinterferometry',["pdspy/interferometry/libinterferometry.pyx"],libraries=["m"],extra_compile_args=['-ffast-math'])])[0]
+#libinterferometry = cythonize("pdspy/interferometry/libinterferometry.pyx")[0]
 libimaging = cythonize("pdspy/imaging/libimaging.pyx")[0]
 bhmie = Extension('pdspy.dust.bhmie', sources=['pdspy/dust/bhmie.f90'])
 bhcoat = Extension('pdspy.dust.bhcoat', sources=['pdspy/dust/bhcoat.f90'])

psheehan/dishes

setup.py
@@ -1,7 +1,8 @@
 from numpy.distutils.core import setup, Extension
 from Cython.Build import cythonize
 
-libinterferometry = cythonize("dishes/interferometry/libinterferometry.pyx")[0]
+libinterferometry = cythonize([Extension('dishes.interferometry.libinterferometry',["dishes/interferometry/libinterferometry.pyx"],libraries=["m"],extra_compile_args=['-ffast-math'])])[0]
+#libinterferometry = cythonize("dishes/interferometry/libinterferometry.pyx")[0]
 libimaging = cythonize("dishes/imaging/libimaging.pyx")[0]
 
 setup(ext_modules=[libinterferometry, libimaging])
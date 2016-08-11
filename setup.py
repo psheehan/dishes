c896154d294e798294c046ccab95790175e43767
Patrick Sheehan <psheehan287@gmail.com>
2016-08-11 14:39:24 -0700

    Made some changes to chisq to make it run significantly faster. Had to update setup.py too to add a compiler flag.

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from numpy.distutils.core import setup, Extension
 from Cython.Build import cythonize
 
-libinterferometry = cythonize("dishes/interferometry/libinterferometry.pyx")[0]
+libinterferometry = cythonize([Extension('dishes.interferometry.libinterferometry',["dishes/interferometry/libinterferometry.pyx"],libraries=["m"],extra_compile_args=['-ffast-math'])])[0]
+#libinterferometry = cythonize("dishes/interferometry/libinterferometry.pyx")[0]
 libimaging = cythonize("dishes/imaging/libimaging.pyx")[0]
 
 setup(ext_modules=[libinterferometry, libimaging])
