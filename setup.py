psheehan/pdspy

Updated a number of codes to be backwards compatible with Python 2.7

setup.py
@@ -3,7 +3,8 @@ from Cython.Build import cythonize
 
 libinterferometry = cythonize([Extension('pdspy.interferometry.libinterferometry',["pdspy/interferometry/libinterferometry.pyx"],libraries=["m"],extra_compile_args=['-ffast-math'])])[0]
 #libinterferometry = cythonize("pdspy/interferometry/libinterferometry.pyx")[0]
-libimaging = cythonize("pdspy/imaging/libimaging.pyx")[0]
+#libimaging = cythonize("pdspy/imaging/libimaging.pyx")[0]
+libimaging = cythonize([Extension('pdspy.imaging.libimaging',["pdspy/imaging/libimaging.pyx"],libraries=[],extra_compile_args=[])])[0]
 bhmie = Extension('pdspy.dust.bhmie', sources=['pdspy/dust/bhmie.f90'])
 bhcoat = Extension('pdspy.dust.bhcoat', sources=['pdspy/dust/bhcoat.f90'])
 

psheehan/dishes

setup.py
@@ -3,6 +3,7 @@ from Cython.Build import cythonize
 
 libinterferometry = cythonize([Extension('dishes.interferometry.libinterferometry',["dishes/interferometry/libinterferometry.pyx"],libraries=["m"],extra_compile_args=['-ffast-math'])])[0]
 #libinterferometry = cythonize("dishes/interferometry/libinterferometry.pyx")[0]
-libimaging = cythonize("dishes/imaging/libimaging.pyx")[0]
+#libimaging = cythonize("dishes/imaging/libimaging.pyx")[0]
+libimaging = cythonize([Extension('dishes.imaging.libimaging',["dishes/imaging/libimaging.pyx"],libraries=[],extra_compile_args=[])])[0]
 
 setup(ext_modules=[libinterferometry, libimaging])
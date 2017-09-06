e9b38619a1c103961f07764888398e848dff7725
Patrick Sheehan <psheehan287@gmail.com>
2017-09-05 19:39:22 -0500

    Updated a number of codes to be backwards compatible with Python 2.7

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from Cython.Build import cythonize
 
 libinterferometry = cythonize([Extension('dishes.interferometry.libinterferometry',["dishes/interferometry/libinterferometry.pyx"],libraries=["m"],extra_compile_args=['-ffast-math'])])[0]
 #libinterferometry = cythonize("dishes/interferometry/libinterferometry.pyx")[0]
-libimaging = cythonize("dishes/imaging/libimaging.pyx")[0]
+#libimaging = cythonize("dishes/imaging/libimaging.pyx")[0]
+libimaging = cythonize([Extension('dishes.imaging.libimaging',["dishes/imaging/libimaging.pyx"],libraries=[],extra_compile_args=[])])[0]
 
 setup(ext_modules=[libinterferometry, libimaging])
